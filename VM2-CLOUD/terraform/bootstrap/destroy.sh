d7547e7a27b6499df4e382b037141723da7a508d
frost_rvl <137229836+frost-rvl@users.noreply.github.com>
2026-05-23 19:49:30 +0100

    fix: fixing apply adn destroy script

--- VM2-CLOUD/terraform/bootstrap/destroy.sh
+++ VM2-CLOUD/terraform/bootstrap/destroy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-export $(grep -v '^#' .env | xargs);
-env | grep "AWS";
+set -a
+source .env
+set +a
 
-if [ $? -ne 0 ]; then
+if ! env | grep -q "^AWS_"; then
     echo "Error: .env file not loaded correctly"
     exit 1
 fi
